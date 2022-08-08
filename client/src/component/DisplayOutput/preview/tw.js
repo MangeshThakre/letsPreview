524b41796dce2c5b232ce697f0553f5a3c5a0a56
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-08-08 14:38:24 +0530

    deploy commit

---
+++ client/src/component/DisplayOutput/preview/tw.js
@@ -0,0 +1,36 @@
+import { margin } from "@mui/system";
+import React from "react";
+function Tw({ image, title, description, url, fullUrl }) {
+  let Newurl = url[1];
+  if (url[1]?.includes("www")) {
+    Newurl = url[1].split("www.")[1];
+  }
+  return (
+    <a href={fullUrl} target="_blank">
+      <div className="twitter">
+        <div>
+          <img src={image} alt="twitter" />
+        </div>
+        <div>
+          <h4
+            className="twitterTitle"
+            style={{ color: "black", fontWeight: "600" }}
+          >
+            {title}
+          </h4>
+          <p
+            className="twitterDescription"
+            style={{ fontSize: "13px", margin: "5px 0" }}
+          >
+            {description?.slice(0, 150)}...
+          </p>
+          <p className="twitterDomain" style={{ color: "gray" }}>
+            {Newurl}
+          </p>
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default Tw;
---
+++ server.js
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import express from "express";
+const app = express();
+import router from "./router/router.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const port = 8081;
+-app.use(bodyParser.json());
+app.use(cors());
+app.use("/api/", router);
+
+// prodiction
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("app is runing successfully");
+  });
+}
+//////
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
