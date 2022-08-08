40f870654a495a7f6ade2f999f79353cea439610
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-08-08 14:37:24 +0530

    beploy commit

--- backend/router/router.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import controller from "../controller/linkcontroller.js";
-const router = express.Router();
-router.get("/url", controller.url);
-export default router;
