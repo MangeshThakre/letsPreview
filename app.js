b30a52a29882aa027d6d1d549092a830072b24a4
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-11-28 15:55:26 +0530

    remove build from client/ginore

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ app.use(express.json());
 app.use(express.json());
 app.use(cors());
 
+// serve static image
+
 // prodiction
+app.use(express.static(path.join(__dirname, "/client/build")));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
   app.use("/api", router);
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
